64fd611c8c1a0d0f248c6491e9562937ab078f84
SparshJain2000 <jainsparsh0801@gmail.com>
2021-04-10 14:04:00 +0530

    connect mongo db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser"),
     express = require("express"),
+    mongoose = require("mongoose"),
     postRouter = require("./routes/post.router"),
     userRouter = require("./routes/user.router"),
     app = express();
@@ -7,8 +8,16 @@ require("dotenv").config();
 app.use(bodyParser.json());
 app.use("/api/post", postRouter);
 app.use("/api/user", userRouter);
-
+const MONGO_URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.8bvsg.mongodb.net/cyber_bully_db?retryWrites=true&w=majority`;
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("connected to MONGO 🤩"))
+    .catch((x) => console.log(x));
 const port = 8080 || process.env.PORT;
 app.listen(port, () => {
-    console.log(`Listening to ${port}`);
+    console.log(`Listening to ${port} `);
 });
