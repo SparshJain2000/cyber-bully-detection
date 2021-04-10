ba30090ca1e6cd4dc201a49b32d200ab9494ccae
SparshJain2000 <jainsparsh0801@gmail.com>
2021-04-10 13:46:53 +0530

    add basic routes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const bodyParser = require("body-parser"),
     express = require("express"),
+    postRouter = require("./routes/post.router"),
+    userRouter = require("./routes/user.router"),
     app = express();
+require("dotenv").config();
 app.use(bodyParser.json());
+app.use("/api/post", postRouter);
+app.use("/api/user", userRouter);
 
 const port = 8080 || process.env.PORT;
 app.listen(port, () => {
