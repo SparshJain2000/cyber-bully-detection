107d9ff815203379c7e0b241091ffd6f375c6ad7
SparshJain2000 <jainsparsh0801@gmail.com>
2021-04-11 01:53:51 +0530

    add multer and upload image

--- routes/post.router.js
+++ routes/post.router.js
@@ -1,5 +1,30 @@
 const router = require("express").Router();
+// var Image = require("../models/image");
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./data/images");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname);
+    },
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        // rejects storing a file
+        cb(null, false);
+    }
+};
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter,
+});
 router.get("/", (req, res) => {
     res.json({
         _id: 123,
@@ -7,7 +32,8 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", upload.single("image"), (req, res) => {
+    console.log(req.file.path);
     const post = {
         title: req.body.title,
         image: req.body.image,
@@ -15,6 +41,6 @@ router.post("/", (req, res) => {
         date: new Date(),
         likes: [],
     };
-    res.json({ status: "posted" });
+    res.json({ status: "posted", image: req.file.path });
 });
 module.exports = router;
